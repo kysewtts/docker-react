5d5cdc83973224ebebf44c0afecc43ab5fdcd947
Ujjwal Prakash <ujjwalprakash3@gmail.com>
2020-04-03 12:22:36 +0530

    added new branch

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>Covid-19</p>
+        <p>This is the feature branch</p>
         <a
           className="App-link"
           href="https://reactjs.org"
